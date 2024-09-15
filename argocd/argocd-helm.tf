7a5da11222c128184325e61eb7f058b240555b80
Mohit Verma <mohitverma@Mohits-Laptop.local>
2024-09-15 18:34:57 +0530

    Adding argocd-v0.0.2 to the repository

--- argocd/argocd-helm.tf
+++ argocd/argocd-helm.tf
@@ -1,7 +1,3 @@
-data "aws_ssm_parameter" "ssh_private_key" {
-  name = var.aws_ssm_key_name
-}
-
 resource "helm_release" "argocd" {
   count = var.enable_argocd ? 1 : 0
 
@@ -18,19 +14,6 @@ resource "helm_release" "argocd" {
   values = [
     templatefile("${path.module}/argocd.yaml", { env = "${var.env}" })
   ]
-
-#  set {
-#    name  = "server.service.annotations[0]"
-#    value = "service.beta.kubernetes.io/aws-load-balancer-type: external"
-#  }
-#  set {
-#    name  = "server.service.annotations[1]"
-#    value = "service.beta.kubernetes.io/aws-load-balancer.scheme: internet-facing"
-#  }
-#  set {
-#    name  = "server.service.annotations[2]"
-#    value = "service.beta.kubernetes.io/aws-load-balancer-type: nlb"
-#  }
 
 }
 
