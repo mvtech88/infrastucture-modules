c9bdf98f945365d92e0243482f4f29eb317addf9
Mohit Verma <mohitverma@Mohits-Laptop.local>
2024-09-28 14:02:45 +0530

    Adding changes to the eks output

--- eks/output.tf
+++ eks/output.tf
@@ -3,9 +3,9 @@ output "eks_name" {
 }
 
 output "openid_provider_arn" {
-  value = aws_iam_openid_connect_provider.this[0].arn
+  value = aws_iam_openid_connect_provider.this[*].arn
 }
 
 output "eks_id" {
-  value = aws_eks_cluster.this[0].id
+  value = aws_eks_cluster.this[*].id
 }
