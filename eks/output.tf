a60a4e223c03799dbc0a1d3924e18606222f6388
Mohit Verma <mohitverma@Mohits-Laptop.local>
2024-09-29 13:07:48 +0530

    Adding changes to the eks cluster output module

--- eks/output.tf
+++ eks/output.tf
@@ -3,7 +3,7 @@ output "eks_name" {
 }
 
 output "openid_provider_arn" {
-  value = aws_iam_openid_connect_provider.this.arn
+  value = aws_iam_openid_connect_provider.this[0].arn
 }
 
 output "eks_id" {
